c064e258935b86d12ec46fd7d4f3a245f5b7e51a
+Sharon <+wanjiku.kahira@gmail.com>
2025-05-12 13:01:03 +0300

    Add payment_method model

---
+++ server/models/payment_method.py
@@ -0,0 +1,23 @@
+import models.db as db
+from datetime import datetime
+from sqlalchemy_serializer import SerializerMixin
+
+class PaymentMethod(db.Model, SerializerMixin):
+
+    """PaymentMethod model for storing payment method information."""
+    
+    __tablename__ = 'payment_methods'
+    serialize_rules = ('-provider_id', '-created_at', '-updated_at','-remittances.payment_method', '-exchange_rates.payment_method', '-provider.payment_methods')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    provider_id = db.Column(db.Integer, db.ForeignKey('mobile_money_prov.id'), nullable=False)
+    type = db.Column(db.String(50), nullable=False) 
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    provider = db.relationship('mobile_money_prov', backref='payment_methods', lazy='select')
+    remittances = db.relationship('Remittance', backref='payment_method', lazy='select')
+
+    def __repr__(self):
+        return f'<PaymentMethod {self.name}>'
