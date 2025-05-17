d74b09622e573983eed84bad81bfe0fb80814a0d
+Sharon <+wanjiku.kahira@gmail.com>
2025-05-17 21:42:42 +0300

    Remove receiver_name and phone

--- server/models/transaction.py
+++ server/models/transaction.py
@@ -12,10 +12,11 @@ class Transaction(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     remittance_id = db.Column(db.Integer, db.ForeignKey('remittances.id'), nullable=False)
-    receiver_name = db.Column(db.String(100), nullable=False)
-    receiver_phone = db.Column(db.String(20), nullable=False)
-    amount_btc = db.Column(db.Numeric(10, 8), nullable=False)
+    # receiver_name = db.Column(db.String(100), nullable=False)
+    # receiver_phone = db.Column(db.String(20), nullable=False)
+    amount_sats = db.Column(db.Numeric(10, 8), nullable=False)
     amount_fiat = db.Column(db.Numeric(10, 2), nullable=False)
     status = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
@@ -23,6 +24,7 @@ class Transaction(db.Model, SerializerMixin):
 
     remittance = db.relationship('Remittance', back_populates='transactions', )
     sender = db.relationship('User', back_populates='transactions', )
+    receiver = db.relationship('User', foreign_keys=[receiver_id], back_populates='received_transactions')
 
     def __repr__(self):
         return f'<Transaction {self.id}>'
